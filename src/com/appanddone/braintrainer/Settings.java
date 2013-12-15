0c8d22ca967f72c632f625c7b32be83afaf2d2bb
Chris Fuller <chris@cjwfuller.com>
2013-12-15 11:57:26 +0000

    Question types basic UI done

---
+++ src/com/appanddone/braintrainer/Settings.java
@@ -0,0 +1,15 @@
+package com.appanddone.braintrainer;
+
+import com.appanddone.braintrainer.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Settings extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+	}
+}
