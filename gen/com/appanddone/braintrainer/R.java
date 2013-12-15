1229230cacd06051c41f939b0dfed5bd1d554f98
Chris Fuller <chris@cjwfuller.com>
2013-12-15 15:14:10 +0000

    Finished settings XML

--- gen/com/appanddone/braintrainer/R.java
+++ gen/com/appanddone/braintrainer/R.java
@@ -24,23 +24,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int classificationQuestionsSwitchButton=0x7f08000b;
+        public static final int classificationQuestionsSwitchButton=0x7f08000f;
         public static final int layout_question_types=0x7f080000;
-        public static final int logicQuestionsSwitchButton=0x7f080008;
-        public static final int mathemticalQuestionsSwitchButton=0x7f08000d;
-        public static final int menu_about=0x7f080010;
-        public static final int menu_settings=0x7f08000f;
-        public static final int patternRecognitionQuestionsSwitchButton=0x7f080009;
-        public static final int spatialQuestionsSwitchButton=0x7f08000a;
-        public static final int text_classification=0x7f080004;
-        public static final int text_logic_questions=0x7f080001;
-        public static final int text_mathematical=0x7f080006;
-        public static final int text_pattern_recognition=0x7f080002;
-        public static final int text_spatial=0x7f080003;
-        public static final int text_verbal=0x7f080007;
-        public static final int text_visualisation=0x7f080005;
-        public static final int verbalQuestionsSwitchButton=0x7f08000e;
-        public static final int visualisationQuestionsSwitchButton=0x7f08000c;
+        public static final int logicQuestionsSwitchButton=0x7f08000c;
+        public static final int mathemticalQuestionsSwitchButton=0x7f080011;
+        public static final int menu_about=0x7f080016;
+        public static final int menu_settings=0x7f080015;
+        public static final int otherSettingsSwitchButton=0x7f080013;
+        public static final int patternRecognitionQuestionsSwitchButton=0x7f08000d;
+        public static final int reset_highscore=0x7f080014;
+        public static final int spatialQuestionsSwitchButton=0x7f08000e;
+        public static final int text_classification=0x7f080005;
+        public static final int text_highscore_reset=0x7f08000b;
+        public static final int text_logic_questions=0x7f080002;
+        public static final int text_mathematical=0x7f080007;
+        public static final int text_other_settings=0x7f080009;
+        public static final int text_pattern_recognition=0x7f080003;
+        public static final int text_question_types=0x7f080001;
+        public static final int text_spatial=0x7f080004;
+        public static final int text_timer=0x7f08000a;
+        public static final int text_verbal=0x7f080008;
+        public static final int text_visualisation=0x7f080006;
+        public static final int verbalQuestionsSwitchButton=0x7f080012;
+        public static final int visualisationQuestionsSwitchButton=0x7f080010;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -50,22 +56,23 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int activity_title_settings=0x7f050005;
+        public static final int activity_title_settings=0x7f050006;
         public static final int app_name=0x7f050000;
-        public static final int classification_questions_switch_text=0x7f05000a;
+        public static final int classification_questions_switch_text=0x7f05000b;
         public static final int hello_world=0x7f050003;
-        public static final int logic_questions_switch_text=0x7f050007;
-        public static final int mathematical_questions_switch_text=0x7f05000b;
+        public static final int logic_questions_switch_text=0x7f050008;
+        public static final int mathematical_questions_switch_text=0x7f05000c;
         public static final int menu_about=0x7f050002;
         public static final int menu_settings=0x7f050001;
-        public static final int pattern_recognition_questions_switch_text=0x7f050008;
+        public static final int pattern_recognition_questions_switch_text=0x7f050009;
         public static final int reset_highscore_button=0x7f050004;
-        public static final int spatial_questions_switch_text=0x7f050009;
-        public static final int timer_switch_text=0x7f050006;
-        public static final int title_other_settings=0x7f05000f;
-        public static final int title_question_types=0x7f05000e;
-        public static final int verbal_questions_switch_text=0x7f05000d;
-        public static final int visualisation_questions_switch_text=0x7f05000c;
+        public static final int reset_highscore_text=0x7f050005;
+        public static final int spatial_questions_switch_text=0x7f05000a;
+        public static final int timer_switch_text=0x7f050007;
+        public static final int title_other_settings=0x7f050010;
+        public static final int title_question_types=0x7f05000f;
+        public static final int verbal_questions_switch_text=0x7f05000e;
+        public static final int visualisation_questions_switch_text=0x7f05000d;
     }
     public static final class style {
         /** 
--- gen/com/example/braintrainer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.braintrainer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
