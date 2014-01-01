4b8635c7b1e0a557bc891e1e7c92aa19987e0c32
Chris Fuller <chris@cjwfuller.com>
2014-01-01 20:55:47 +0000

    Added ads to finish activity

--- src/com/appanddone/braintrainer/Finish.java
+++ src/com/appanddone/braintrainer/Finish.java
@@ -1,5 +1,8 @@
 package com.appanddone.braintrainer;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -9,6 +12,10 @@ public class Finish extends MainActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_finish);
+		
+		AdView adView = (AdView)this.findViewById(R.id.adView);
+	    AdRequest adRequest = new AdRequest.Builder().build();
+	    adView.loadAd(adRequest);
 		
 		BrainTrainer brainTrainer = (BrainTrainer)getApplicationContext();
 		TextView correct = (TextView)findViewById(R.id.correct_answers_text);
