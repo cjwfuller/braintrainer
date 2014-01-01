0c3563723616b88f9318fe81c693915311d8e4ed
Chris Fuller <chris@cjwfuller.com>
2014-01-01 15:21:28 +0000

    Lives blackout in maths activity

--- src/com/appanddone/braintrainer/Mathematics.java
+++ src/com/appanddone/braintrainer/Mathematics.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.*;
@@ -20,6 +21,7 @@ public class Mathematics extends MainActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mathematics);
+		showLives();
 		
 		AdView adView = (AdView)this.findViewById(R.id.adView);
 	    AdRequest adRequest = new AdRequest.Builder().build();
@@ -45,6 +47,22 @@ public class Mathematics extends MainActivity {
 	@Override
 	public void onBackPressed() {
 		super.onBackPressed();
+	}
+	
+	/**
+	 * Show lives - users have 3 lives
+	 * Red hearts are available lives
+	 * Black hearts are used lives
+	 */
+	private void showLives() {
+		ImageView heart1 = (ImageView)findViewById(R.id.heart1);
+		ImageView heart2 = (ImageView)findViewById(R.id.heart2);
+		if(brainTrainer.numIncorrect == 1) {
+			heart1.setImageResource(R.drawable.black_heart);
+		} else if(brainTrainer.numIncorrect == 2) {
+			heart1.setImageResource(R.drawable.black_heart);
+			heart2.setImageResource(R.drawable.black_heart);
+		}
 	}
 	
 	private void setProblems() {
